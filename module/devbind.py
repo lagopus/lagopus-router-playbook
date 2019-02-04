794fa40270e31c99b5654cc5199fba7c66b8acb3
Ryo Nakamura <upa@haeena.net>
2019-02-04 23:36:15 +0900

    writing devbind

---
+++ module/devbind.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+
+import os
+
+SYS_DEVICES = "/sys/bus/pci/devices"
+SYS_DRIVERS = "/sys/bus/pci/drivers"
+
+class Devbind(object):
+
+    def __init__(self, module):
+
+        self.module = module
+        self.device = module.params["device"]
+        self.driver = module.params["driver"]
+        self.sys_device_path = "{}/{}".format(SYS_DEVICES, self.device)
+        self.sys_driver_path = "{}/{}".format(SYS_DRIVERS, self.driver)
+
+        if self.driver == "None":
+            self.driver = None # not bind to any drivers
+
+        # validate
+        if not os.path.exists(self.sys_device_path):
+            msg = "deivce {} does not exist".format(self.device)
+            self.module.fail_json(msg = msg)
+
+        if not os.path.exists(self.sys_driver_path):
+            msg = "driver {} does not exist".format(self.driver)
+            self.module.fail_json(msg = msg)
+
+
+    def get_current_driver(self):
+
+        driver_path = "{}/{}".format(self.sys_device_path, "drvier")
+        if not os.path.exists(driver_path):
+            return None
+        else:
+            path = os.readlink()
+            return os.path.basename(path)
+
+    def will_changed(self):
+
+        if self.driver != self.get_current_driver():
+            return True
+        else:
+            return False
+        
+
+    def check(self):
+        if self.will_changed():
+            self.module.exit_json(changed = True)
+        else:
+            self.module.exit_json(changed = False)
+
+
+    def sysfs_write(self, path, value):
+
+        try:
+            f = open(path, "w")
+        except Exception as e:
+            msg = "failed to open {}: {}".format(path, e)
+            self.module.fail_json(msg = msg)
+            
+        f.write(value)
+        f.close()
+
+
+    def run(self):
+
+        if not self.will_changed():
+            self.module_exit_json(changed = False)
+            
+        # unbind
+        if self.get_current_driver():
+            unbind_path = "{}/driver/unbind".format(self.sys_device_path)
+            self.sysfs_write(unbind_path, self.device)
+        
+        # bind
+        bind_path = "{}/bind".format(self.sys_driver_path)
+        raise Exception(bind_path)
+        self.sysfs_write(bind_path, self.device)
+
+
+def main():
+
+    module = AnsibleModule(
+        argument_spec = dict(
+            device = dict(required = True),
+            driver = dict(required = True),
+        ),
+        supports_check_mode = True
+    )
+
+    devbind = Devbind(module)
+
+    if module.check_mode:
+        devbind.check()
+    else:
+        devbind.run()
+
+
+from ansible.module_utils.basic import *
+main()
